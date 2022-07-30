74e18a1705c105a6af7eb4e2699b2070c77a41de
howayi <fov223@gmail.com>
2022-07-30 15:06:35 +0800

    route root to card_list

--- cards/urls.py
+++ cards/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from django.views.generic import TemplateView
+
+from . import views
 
 urlpatterns = [
     path(
         "",
-        TemplateView.as_view(template_name="cards/base.html"),
-        name="home"
+        views.CardListView.as_view(template_name="cards/cards_list.html"),
+        name="card-list"
     ),
 ]
