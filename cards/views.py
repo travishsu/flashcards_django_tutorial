3db7c4ae84c939f1af73040a07cd0ace852b355f
howayi <fov223@gmail.com>
2022-07-30 14:56:18 +0800

    add class-based view of cards

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+)
+
+from .models import Card
 
 # Create your views here.
+
+
+class CardListView(ListView):
+    model = Card
+    queryset = Card.objects.all().order_by("box", "-date_created")
